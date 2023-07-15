6f8c05a12178009620699f1e993f2f19644cc9fb
Laudier2 <trabalhandooline2019@hotmail.com>
2023-07-14 23:26:49 -0300

    first commit

---
+++ src/components/Index.jsx
@@ -0,0 +1,169 @@
+import React from 'react'
+
+export default function Index() {
+  return (      
+    <div class="container">
+        <div class="card-deck mt-5">
+            <div class="card">
+                <a href="https://multijogos.net/assistir-athletico-pr-x-flamengo-ao-vivo-online-hd-12-07-2023/" target="_blank" rel="noopener noreferrer">
+                    <img class="card-img-top rounded " src="f1.jpg" alt="img"/>
+                    
+                </a>
+            <div class="card-body">
+                
+                <p class="card-title font-weight-bold">
+                    Assistir Athletico-PR x Flamengo Ao Vivo Online HD 12/07/2023
+                </p>
+            </div>
+            </div>
+            <div class="card">
+                <a href="https://multijogos.net/assistir-athletico-pr-x-flamengo-ao-vivo-online-hd-12-07-2023/" target="_blank" rel="noopener noreferrer">
+                    <img class="card-img-top rounded " src="https://multijogos.net/wp-content/uploads/2023/07/Athletico-PR-x-Flamengo-Ao-Vivo-Online-360x180.jpg" alt="img"/>
+                    
+                </a>
+            <div class="card-body">
+                
+                <p class="card-text">
+                    Assistir Independiente Medellín x San Lorenzo Ao Vivo Online HD 12/07/2023
+                </p>
+            
+            </div>
+            </div>
+            <div class="card">
+                <a href="https://multijogos.net/assistir-athletico-pr-x-flamengo-ao-vivo-online-hd-12-07-2023/" target="_blank" rel="noopener noreferrer">
+                    <img class="card-img-top rounded " src="https://multijogos.net/wp-content/uploads/2023/07/Athletico-PR-x-Flamengo-Ao-Vivo-Online-360x180.jpg" alt="img"/>
+                    
+                </a>
+            <div class="card-body">
+                
+                <p class="card-title font-weight-bold">
+                    Assistir Sporting Cristal x Emelec Ao Vivo Online HD 12/07/2023
+                </p>
+                
+            </div>
+            </div>
+            <div class="card">
+                <a href="https://multijogos.net/assistir-athletico-pr-x-flamengo-ao-vivo-online-hd-12-07-2023/" target="_blank" rel="noopener noreferrer">
+                    <img class="card-img-top rounded " src="https://multijogos.net/wp-content/uploads/2023/07/Athletico-PR-x-Flamengo-Ao-Vivo-Online-360x180.jpg" alt="img"/>
+                    
+                </a>
+                <div class="card-body">
+                
+                <p class="card-title font-weight-bold">
+                    Assistir Sporting Cristal x Emelec Ao Vivo Online HD 12/07/2023
+                </p>
+                
+                </div>
+            </div>
+        </div>
+        <div class="card-deck mt-5">
+            <div class="card">
+                <a href="https://multijogos.net/assistir-athletico-pr-x-flamengo-ao-vivo-online-hd-12-07-2023/" target="_blank" rel="noopener noreferrer">
+                    <img class="card-img-top rounded " src="https://multijogos.net/wp-content/uploads/2023/07/Athletico-PR-x-Flamengo-Ao-Vivo-Online-360x180.jpg" alt="img"/>
+                    
+                </a>
+            <div class="card-body">
+                
+                <p class="card-title font-weight-bold">
+                    Assistir Athletico-PR x Flamengo Ao Vivo Online HD 12/07/2023
+                </p>
+                
+            </div>
+            </div>
+            <div class="card">
+                <a href="https://multijogos.net/assistir-athletico-pr-x-flamengo-ao-vivo-online-hd-12-07-2023/" target="_blank" rel="noopener noreferrer">
+                    <img class="card-img-top rounded " src="https://multijogos.net/wp-content/uploads/2023/07/Athletico-PR-x-Flamengo-Ao-Vivo-Online-360x180.jpg" alt="img"/>
+                    
+                </a>
+            <div class="card-body">
+               
+                <p class="card-title font-weight-bold">
+                    Assistir Independiente Medellín x San Lorenzo Ao Vivo Online HD 12/07/2023
+                </p>
+            
+            </div>
+            </div>
+            <div class="card">
+                <a href="https://multijogos.net/assistir-athletico-pr-x-flamengo-ao-vivo-online-hd-12-07-2023/" target="_blank" rel="noopener noreferrer">
+                    <img class="card-img-top rounded " src="https://multijogos.net/wp-content/uploads/2023/07/Athletico-PR-x-Flamengo-Ao-Vivo-Online-360x180.jpg" alt="img"/>
+                    
+                </a>
+            <div class="card-body">
+                
+                <p class="card-title font-weight-bold">
+                    Assistir Sporting Cristal x Emelec Ao Vivo Online HD 12/07/2023
+                </p>
+                
+            </div>
+            </div>
+            <div class="card">
+                <a href="https://multijogos.net/assistir-athletico-pr-x-flamengo-ao-vivo-online-hd-12-07-2023/" target="_blank" rel="noopener noreferrer">
+                    <img class="card-img-top rounded " src="https://multijogos.net/wp-content/uploads/2023/07/Athletico-PR-x-Flamengo-Ao-Vivo-Online-360x180.jpg" alt="img"/>
+                    
+                </a>
+                <div class="card-body">
+                
+                <p class="card-title font-weight-bold">
+                    Assistir Sporting Cristal x Emelec Ao Vivo Online HD 12/07/2023
+                </p>
+                
+                </div>
+            </div>
+        </div>
+        <div class="card-deck mt-5">
+            <div class="card">
+                <a href="https://multijogos.net/assistir-athletico-pr-x-flamengo-ao-vivo-online-hd-12-07-2023/" target="_blank" rel="noopener noreferrer">
+                    <img class="card-img-top rounded " src="https://multijogos.net/wp-content/uploads/2023/07/Athletico-PR-x-Flamengo-Ao-Vivo-Online-360x180.jpg" alt="img"/>
+                    
+                </a>
+            <div class="card-body">
+                
+                <p class="card-title font-weight-bold">
+                    Assistir Athletico-PR x Flamengo Ao Vivo Online HD 12/07/2023
+                </p>
+                
+            </div>
+            </div>
+            <div class="card">
+                <a href="https://multijogos.net/assistir-athletico-pr-x-flamengo-ao-vivo-online-hd-12-07-2023/" target="_blank" rel="noopener noreferrer">
+                    <img class="card-img-top rounded " src="https://multijogos.net/wp-content/uploads/2023/07/Athletico-PR-x-Flamengo-Ao-Vivo-Online-360x180.jpg" alt="img"/>
+                    
+                </a>
+            <div class="card-body">
+                
+                <p class="card-title font-weight-bold">
+                    Assistir Independiente Medellín x San Lorenzo Ao Vivo Online HD 12/07/2023
+                </p>
+            
+            </div>
+            </div>
+            <div class="card">
+                <a href="https://multijogos.net/assistir-athletico-pr-x-flamengo-ao-vivo-online-hd-12-07-2023/" target="_blank" rel="noopener noreferrer">
+                    <img class="card-img-top rounded " src="https://multijogos.net/wp-content/uploads/2023/07/Athletico-PR-x-Flamengo-Ao-Vivo-Online-360x180.jpg" alt="img"/>
+                    
+                </a>
+            <div class="card-body">
+               
+                <p class="card-title font-weight-bold">
+                    Assistir Sporting Cristal x Emelec Ao Vivo Online HD 12/07/2023
+                </p>
+                
+            </div>
+            </div>
+            <div class="card">
+                <a href="https://multijogos.net/assistir-athletico-pr-x-flamengo-ao-vivo-online-hd-12-07-2023/" target="_blank" rel="noopener noreferrer">
+                    <img class="card-img-top rounded " src="https://multijogos.net/wp-content/uploads/2023/07/Athletico-PR-x-Flamengo-Ao-Vivo-Online-360x180.jpg" alt="img"/>
+                    
+                </a>
+                <div class="card-body">
+                <h5 class="card-title font-weight-bold">Futebol ao vivo</h5>
+                <p class="card-text">
+                    Assistir Sporting Cristal x Emelec Ao Vivo Online HD 12/07/2023
+                </p>
+                
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
